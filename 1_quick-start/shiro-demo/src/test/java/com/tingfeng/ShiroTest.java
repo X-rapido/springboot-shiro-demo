6897d828e6e833bcfbfedfc904eb3ef6c5f1e022
liurenkui <liurenkui@wusongtech.com>
2018-04-27 19:50:25 +0800

    非常简单的ini配置测试

---
+++ 1_quick-start/shiro-demo/src/test/java/com/tingfeng/ShiroTest.java
@@ -0,0 +1,46 @@
+package com.tingfeng;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShiroTest {
+
+    @Test
+    public void testIniRealm() {
+        // 1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<SecurityManager> factroy = new IniSecurityManagerFactory("classpath:shiro.ini");
+
+        // 2、得到SecurityManager实例并绑定给SecurityUtils
+        SecurityManager securityManager = factroy.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        // 3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("tingfeng", "123456");
+        try {
+            // 4、登录，即身份验证
+            subject.login(token);
+
+            System.out.printf("用户/主体，登录状态：%s \n", subject.isAuthenticated());
+
+        } catch (AuthenticationException e) {
+            // 5、身份验证失败
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+
+        // 6、退出
+        subject.logout();
+
+        System.out.printf("用户/主体，登录状态：%s", subject.isAuthenticated());
+    }
+
+}
